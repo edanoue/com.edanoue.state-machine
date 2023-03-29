c2186b5bbda97bd4e0618e6ba671ca7c36be2dc6
Minami Tomonobu <minami@edanoue.com>
2023-03-29 20:08:50 +0900

    :truck: Rename Condition to If

--- Runtime/HybridGraph/BehaviourTree/Decorators/If.cs
+++ Runtime/HybridGraph/BehaviourTree/Decorators/If.cs
@@ -7,24 +7,24 @@ namespace Edanoue.HybridGraph
 {
     public static class ConditionExtensions
     {
-        public static IDecoratorNode Condition<T>(this IDecoratorPort self, Func<T, bool> condition)
+        public static IDecoratorNode If<T>(this IDecoratorPort self, Func<T, bool> condition)
         {
-            return self.Condition(condition, "Condition");
+            return self.If(condition, "Condition");
         }
 
-        public static IDecoratorNode Condition<T>(this IDecoratorPort self, Func<T, bool> condition, string name)
+        public static IDecoratorNode If<T>(this IDecoratorPort self, Func<T, bool> condition, string name)
         {
-            var node = new BtDecoratorNodeCondition<T>(condition, name);
+            var node = new BtDecoratorNodeIf<T>(condition, name);
             self.AddDecorator(node);
             return node;
         }
     }
 
-    internal sealed class BtDecoratorNodeCondition<T> : BtDecoratorNode
+    internal sealed class BtDecoratorNodeIf<T> : BtDecoratorNode
     {
         private readonly Func<T, bool> _condition;
 
-        internal BtDecoratorNodeCondition(Func<T, bool> condition, string name) : base(name)
+        internal BtDecoratorNodeIf(Func<T, bool> condition, string name) : base(name)
         {
             _condition = condition;
         }
