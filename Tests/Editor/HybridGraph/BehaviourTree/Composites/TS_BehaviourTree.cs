fbe5cbca4dfbf01f102bb25642f88856f80f5081
Minami Tomonobu <minami@edanoue.com>
2023-03-30 16:42:26 +0900

    :white_check_mark: テストの追加

--- Tests/Editor/HybridGraph/BehaviourTree/Composites/TS_BehaviourTree.cs
+++ Tests/Editor/HybridGraph/BehaviourTree/Composites/TS_BehaviourTree.cs
@@ -9,11 +9,19 @@ namespace Edanoue.HybridGraph.Tests
     public class TS_BehaviourTree
     {
         [Test]
-        public void BehaviourTreeの動作確認()
+        public void BehaviourTreeをBehaviourTreeに埋め込める()
         {
             var blackboard = new MockBlackboard();
             var graph = EdaGraph.Run<MockBtA>(blackboard);
             Assert.That(blackboard.Counter, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void SubBehaviourTreeが失敗する()
+        {
+            var blackboard = new MockBlackboard();
+            var graph = EdaGraph.Run<MockBtB>(blackboard);
+            Assert.That(blackboard.Counter, Is.EqualTo(3));
         }
 
         private class MockBlackboard
@@ -51,6 +59,46 @@ namespace Edanoue.HybridGraph.Tests
             }
 
             private class MockBtASubBtB : BehaviourTree<MockBlackboard>
+            {
+                protected override void OnSetupBehaviours(IRootNode root)
+                {
+                    var sequence = root.Add.Sequence();
+                    sequence.Add.Action<MockBlackboard>(CountUpAction);
+                    sequence.Add.Action<MockBlackboard>(CountUpAction);
+                }
+            }
+        }
+
+        private class MockBtB : BehaviourTree<MockBlackboard>
+        {
+            // Blackboard のカウンターを一つ進める Action
+            private static bool CountUpAction(MockBlackboard bb)
+            {
+                bb.Counter++;
+                return true;
+            }
+
+            protected override void OnSetupBehaviours(IRootNode root)
+            {
+                var selector = root.Add.Selector();
+                // SubBehaviourTree を追加する
+                selector.Add.BehaviourTree<MockSubBtA>(); // 1回カウントアップ(途中で失敗する)
+                selector.Add.BehaviourTree<MockSubBtB>(); // 2回カウントアップ
+            }
+
+            private class MockSubBtA : BehaviourTree<MockBlackboard>
+            {
+                protected override void OnSetupBehaviours(IRootNode root)
+                {
+                    var sequence = root.Add.Sequence();
+                    sequence.Add.Action<MockBlackboard>(CountUpAction);
+                    sequence.Add.Action(() => false);
+                    sequence.Add.Action<MockBlackboard>(CountUpAction);
+                    sequence.Add.Action<MockBlackboard>(CountUpAction);
+                }
+            }
+
+            private class MockSubBtB : BehaviourTree<MockBlackboard>
             {
                 protected override void OnSetupBehaviours(IRootNode root)
                 {
